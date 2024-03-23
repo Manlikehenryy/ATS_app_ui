18060ae2618819a0f9a869319ab68537f2da953c
royalexchangegeneral <75024044+royalexchangegeneral@users.noreply.github.com>
2024-03-23 01:38:30 +0100

    added position

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,9 +8,9 @@ import Navbar from '../components/Navbar.vue';
   <div style="margin-top: -100px;" class="bg-white">
 
     <!-- <h6 v-if="!jobs">Loading...</h6> -->
-    <div style="margin: auto;">
-      <h6 style="margin-top:300px; text-align: center;">Loading...</h6>
-    </div>
+    <!-- <div style="margin: auto;"> -->
+      <h6 style="position: absolute; transform: translateX(-50%);">Loading...</h6>
+    <!-- </div> -->
     
   <!-- <div v-for="job in jobs" :key="job.id" style="margin-bottom: -220px;"  class="relative isolate px-6  lg:px-8">
   <RouterLink :to="{name: 'viewJob', params:{id:job.id}}">
