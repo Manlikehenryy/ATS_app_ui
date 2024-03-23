9304b5784beb394b7a98b3121d8d03c8f9b6c8fc
royalexchangegeneral <75024044+royalexchangegeneral@users.noreply.github.com>
2024-03-23 02:18:01 +0100

    header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ import Navbar from '../components/Navbar.vue';
   <div style="margin-top: -100px;" class="bg-white">
     
       <div v-if="showSpinner" class="container"> <div class="spinner loading loading-spinner"></div></div>
-      <div v-if="!jobs" class="container"> <h3>No jobs available!</h3></div>
+      <div v-if="!jobs" class="container"> <h3 >No jobs available!</h3></div>
   <div v-for="job in jobs" :key="job.id" style="margin-bottom: -220px;"  class="relative isolate px-6  lg:px-8">
   <RouterLink :to="{name: 'viewJob', params:{id:job.id}}">
   
@@ -43,6 +43,15 @@ import Navbar from '../components/Navbar.vue';
   height: 100vh; /* Adjust as needed */
 }
 
+.container h3 {
+  width: 3rem;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 100%; /* Ensure the image doesn't exceed the container's width */
+  max-height: 100%; /* Ensure the image doesn't exceed the container's height */
+}
 .container .spinner {
   width: 3rem;
   position: absolute;
